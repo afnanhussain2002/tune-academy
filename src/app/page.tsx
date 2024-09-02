8bec6ddfce91dc40b2e466a7fdea84064cadca3d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-02 11:13:08 +0600

    complete the navbar work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div className='text-center text-3xl font-bold'>
+   <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
+     <h1 className='text-center text-3xl font-bold mt-5'>
       TuneAcademy
-    </div>
+    </h1>
+   </main>
   );
 };
 
